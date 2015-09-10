fbfea08a691b56ddf8ea0d47bcd202128e8a45ca
feefles <eyeung@seas.upenn.edu>
2015-09-09 23:38:05 -0400

    initial commit of chrome extension

---
+++ piazza-analytics-chrome/src/inject.js
@@ -0,0 +1,25 @@
+var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+        $(window).hashchange(updateName);
+
+        updateName()
+       
+    }
+}, 50);
+
+function updateNames() {
+    var server_ip = "http://159.203.71.54/";
+        //this should be well formed
+    var post_id = window.location.search.split("=")[1]
+    $.ajax({
+        type:'GET', 
+        url: server_ip + "tag_good/" + post_id, 
+        dataType: 'json', 
+    }).done(function(data) {
+        div = "<div >" + data["tag_good"].join() + "</div>"; 
+        $('.do_good_note').after(div) ;
+        div = "<div>" + data["tag_endorse"].join() + "</div>"; 
+        $('.do_good_answer').after(div);
+    });
+}

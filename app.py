05b403318ed67546e56eb17e54d5552b64cd57fb
feefles <eyeung@seas.upenn.edu>
2015-09-12 12:54:38 -0400

    change port to 80

--- app.py
+++ app.py
@@ -83,5 +83,5 @@ def get_person(class_id, post_id):
     return json.dumps(d)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 80))
     app.run(host='0.0.0.0', port=port)

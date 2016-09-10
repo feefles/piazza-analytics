556a9b203b05de59cd21f846a90988851714a63f
Evelyn Fifi Yeung <eyeung@seas.upenn.edu>
2016-09-10 17:48:50 -0400

    add homework viz filtering

--- viz_app/app.py
+++ viz_app/app.py
@@ -1,9 +1,9 @@
 import os
 
 import flask
-from flask import render_template
+from flask import render_template, request
 
-from viz_app import scrape_course
+import scrape_course
 
 app = flask.Flask(__name__)
 
@@ -15,8 +15,22 @@ def index():
 @app.route('/graph', methods=['GET'])
 def returnGraphData():
     graph = scrape_course.main()
-    nodes = [{'data': {'id': id, 'postContent': post.post_content}} for (id, post) in graph.iteritems() if len(post.outlinks + post.inlinks) != 0]
+    nodes = [{'data': {'id': id, 'postContent': post.post_content, 'postAnswer': post.post_instructor_answer}} for (id, post) in graph.iteritems() if len(post.outlinks + post.inlinks) != 0]
     edges = [{'data': {'source': str(post.post_number), 'target': outlink}} for post in graph.values() for outlink in post.outlinks]
+    return render_template('index.html', nodes=nodes, edges=edges)
+
+
+@app.route('/graphHW', methods=['POST'])
+def returnGraphDataHW():
+    hw = request.form['homework']
+    graph = scrape_course.main()
+    nodes = [{'data': {'id': id, 'postContent': post.post_content, 'postAnswer': post.post_instructor_answer}}
+             for (id, post) in graph.iteritems()
+             if len(post.outlinks + post.inlinks) != 0 and
+             hw in post.folders]
+    edges = [{'data': {'source': str(post.post_number), 'target': outlink}}
+             for post in graph.values() for outlink in post.outlinks
+             if hw in post.folders or hw in graph[outlink].folders]
     return render_template('index.html', nodes=nodes, edges=edges)
 
 
